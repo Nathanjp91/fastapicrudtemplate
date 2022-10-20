df47e116cffc47be5183ec2073169dd75754acf7
Nathan Perkins <nathanjp91@gmail.com>
2022-10-20 18:15:33 +0100

    hello, world

---
+++ fastapicrudtemplate/app.py
@@ -0,0 +1,19 @@
+""" Main application logic"""
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=5000, log_level="info", reload=True)
---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+""" Test suite for app """
+import pytest_check as check
+
+
+def test_hello_world():
+    """Hello World test suite"""
+    check.equal(1, 1)
